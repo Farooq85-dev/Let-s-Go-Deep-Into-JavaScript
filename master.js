ec95191e161328be5bd6b506be253e0448ad84f3
Muhammad Farooq <famuhammad907@gmail.com>
2024-06-03 08:55:29 +0500

    Stack Memory

--- master.js
+++ master.js
@@ -3,3 +3,12 @@
 /* It is an intersting concept in JavaScript. Let's break down this by 
 following examples. */
 
+/* In Simple Words, Primitive Data types used Stack Memory. Primitive values are immutable
+(can't be changed). However, we are changing it's copy value not it's original value. That's why,
+we are accessing "Muhammad Azam" still in the console. */
+
+let name1 = "Muhammad Azam";
+console.log(name1); //Excepted Output will be "Muhammad Azam"
+name1 = "Muhammad Farooq";
+console.log(name1); //Excepted Output will be "Muhammad Farooq"
+

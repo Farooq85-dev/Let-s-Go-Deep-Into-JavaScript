0b04a82eecc270105782905494e7957fe8bae96f
Muhammad Farooq <famuhammad907@gmail.com>
2024-06-03 09:09:59 +0500

    Stack & Heap End

--- master.js
+++ master.js
@@ -4,11 +4,29 @@
 following examples. */
 
 /* In Simple Words, Primitive Data types used Stack Memory. Primitive values are immutable
-(can't be changed). However, we are changing it's copy value not it's original value. That's why,
-we are accessing "Muhammad Azam" still in the console. */
+(can't be changed). However, we are changing it's copy value not it's original value. */
 
 let name1 = "Muhammad Azam";
+let name2 = name1;
+name2 = "Muhammad Farooq";
 console.log(name1); //Excepted Output will be "Muhammad Azam"
-name1 = "Muhammad Farooq";
-console.log(name1); //Excepted Output will be "Muhammad Farooq"
+console.log(name2); //Excepted Output will be "Muhammad Farooq"
 
+/* In Simple Word's, Non-Primitive data types are using Heap memory.They are mutable
+(can be changed).  */
+
+let obj1 = {
+    name: "Muhammad Arslan",
+    class: "WAM-5",
+    section: "A+",
+}
+
+let obj2 = obj1;
+
+obj2.name = "Muhammad Faiq";
+obj2.class = "WAM-10";
+obj2.section = "B+";
+console.log(obj1); // Intersting concept here. Please, note here.
+console.log(obj2); // Intersting concept here. Please, note here.
+
+// ********** The End ********** //
